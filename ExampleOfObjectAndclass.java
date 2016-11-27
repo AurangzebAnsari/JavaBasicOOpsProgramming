15d1d43322b73f2612e9f9a32c4c7c200fa2216b
AurangzebAnsari <ansari95904@gmail.com>
2016-11-27 13:50:35 -0800

    Simple Example of class and object with initilized values
    In this Example We have created a ExampleOfObjectAndclass (Class). and
    That have three instance variable, (Instance variable: A variable that
    is created inside of class But outside the method. it is called Instance
    variable.)
    it is also called data member of class. there are  id, name, marks, Id
    is int type variable.name is String type variable. and marks is double
    type variable.
    and that have two method. one is parameterize and second is simple
    method. After that we have used main method of class. and we have
    created the object of the ExampleOfObjectAndclass(class) by new
    keyword(The new keyword is used to allocate memory at runtime) here
    creating two object of the class. and printing the object value. In this
    Program values are  printed becouse values are initized. so these are
    printed Rolll number,Student name, marks . you have one think. in this
    program. i took a method parametrized. how value is print. you see.
    object is an instance variable of class.
    object gets the memory in heap area and reference variable refers to the
    object allocated in heap memory area. Here example1 and example2 both
    are reference variable that refers to the object allocated in memory.

---
+++ ExampleOfObjectAndclass.java
@@ -0,0 +1,37 @@
+package ClassAndObject;
+
+public class ExampleOfObjectAndclass {
+
+	//it is data mamber and also instance variable
+
+		int rollno;
+		String name;
+		double marks; 
+		void insertRecord(int r, String n,double m)
+		{
+			rollno=r;
+			name=n;
+			marks=m;
+		}
+		void display()
+		{
+			System.out.println("Roll number    "+rollno+"\nStudent'sName  "+name+"\nStudent's Marks "+marks);
+		}
+		public static void main(String args[])
+		{
+                          //creating an object of class (ExampleOfObjectAndclass)
+  
+                        ExampleOfObjectAndclass example1=new ExampleOfObjectAndclass();
+			example1.insertRecord(12,"Rehan Raza", 78.8);
+			example1.display();
+			
+                        //creating second object of class (ExampleOfObjectAndclass)
+
+			ExampleOfObjectAndclass example2=new ExampleOfObjectAndclass();
+			example2.insertRecord(13,"Suraj Kumar", 68.9);
+			example2.display();
+			
+			
+		}
+
+}
